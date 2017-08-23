6becea070431ea7b25beabc86f976b16ad320da4
孟政元 <278227739@qq.com>
2017-08-23 22:50:29 +0800

    /getLocation

--- src/main/java/com/vanging/www/yoyo/restful/servlet/GetLocation.java
+++ src/main/java/com/vanging/www/yoyo/restful/servlet/GetLocation.java
@@ -1,30 +1,41 @@
 package com.vanging.www.yoyo.restful.servlet;
 
 import com.alibaba.fastjson.JSON;
+import com.vanging.www.yoyo.persistence.Action;
 import com.vanging.www.yoyo.restful.response.Response;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet("/getLocation")
 public class GetLocation extends HttpServlet
 {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
         Response finalResponse = new Response();
 
-        String param = request.getParameter("param");
-        String param1 = request.getParameter("param1");
+        String user_id = request.getParameter("user_id");
 
-        if(param == null || param1 == null)
+        if(user_id == null)
         {
             finalResponse.setStatus("param_wrong");
         }
         else
         {
-
+            String location = Action.selectLocation(user_id);
+            if(location == null)
+            {
+                finalResponse.setStatus("not_exist");
+            }
+            else
+            {
+                finalResponse.setStatus("ok");
+                finalResponse.setMessage(location);
+            }
         }
 
         JSON.writeJSONString(response.getWriter(), finalResponse);
