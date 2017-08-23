0c65a008fb6573e29857bfa65c50a827ad0185d7
孟政元 <278227739@qq.com>
2017-08-23 23:07:29 +0800

    /setLocation

--- src/main/java/com/vanging/www/yoyo/restful/servlet/SetLocation.java
+++ src/main/java/com/vanging/www/yoyo/restful/servlet/SetLocation.java
@@ -1,30 +1,44 @@
 package com.vanging.www.yoyo.restful.servlet;
 
 import com.alibaba.fastjson.JSON;
+import com.vanging.www.yoyo.persistence.Action;
+import com.vanging.www.yoyo.persistence.entity.Profile;
 import com.vanging.www.yoyo.restful.response.Response;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet("/setLocation")
 public class SetLocation extends HttpServlet
 {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
         Response finalResponse = new Response();
 
-        String param = request.getParameter("param");
-        String param1 = request.getParameter("param1");
+        String user_id = request.getParameter("user_id");
+        String user_location = request.getParameter("user_location");
 
-        if(param == null || param1 == null)
+        if(user_id == null || user_location == null)
         {
             finalResponse.setStatus("param_wrong");
         }
         else
         {
-
+            Profile profile = new Profile();
+            profile.setUser_id(user_id);
+            profile.setUser_location(user_location);
+            if(Action.updateLocation(profile))
+            {
+                finalResponse.setStatus("ok");
+            }
+            else
+            {
+                finalResponse.setStatus("db_error");
+            }
         }
 
         JSON.writeJSONString(response.getWriter(), finalResponse);
